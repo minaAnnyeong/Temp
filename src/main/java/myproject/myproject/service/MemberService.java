556546665e9f24fef2745ae40aa1157c8f3e44db
minaAnnyeong <minaya257@gmail.com>
2025-06-03 12:51:22 +0900

    User Log in Added

--- src/main/java/myproject/myproject/service/MemberService.java
+++ src/main/java/myproject/myproject/service/MemberService.java
@@ -55,8 +55,11 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    // 개별 회원 조회
+    // 개별 회원 조회 - by id
     public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
     }
+
+    // 개별 회원 조회 - by email
+    public Optional<Member> findByEmail(String email) { return this.memberRepository.findByEmail(email); }
 }
