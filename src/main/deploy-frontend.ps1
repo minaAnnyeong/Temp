4a035f71a1e1030bda24dd43709915b28eb5350e
minaAnnyeong <minaya257@gmail.com>
2025-06-04 09:30:30 +0900

    Apply .gitignore

--- src/main/deploy-frontend.ps1
+++
@@ -1,7 +0,0 @@
-cd frontend
-npm run build
-
-Remove-Item -Recurse -Force ../MyProject/src/main/resources/static/*
-Copy-Item -Recurse -Force build/* ../MyProject/src/main/resources/static/
-
-Write-Host "✅ React build deployed to Spring Boot static folder."
